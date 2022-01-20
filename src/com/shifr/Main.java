4e6c4634ccc01fca04411de56632770e0dad6ba2
Ihor Nikolaiev <igorexa.ua@gmail.com>
2022-01-20 23:33:35 +0200

    Add while and other

---
+++ src/com/shifr/Main.java
@@ -0,0 +1,20 @@
+package com.shifr;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int key = 7;
+        String alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ.,\"\":- ?!;";
+
+
+        while (true){
+            System.out.println("If you want to encrypt? Please press - 1");
+            System.out.println("Enter the key - ");
+            System.out.println("If you want to decrypt? Please press - 2");
+            System.out.println("Enter the key - ");
+            if (1)
+
+        }
+    }
+}
