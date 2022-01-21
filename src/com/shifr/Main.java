db0d438cca89e43480a94f5c4c4856642ab133b6
Ihor Nikolaiev <igorexa.ua@gmail.com>
2022-01-21 23:42:10 +0200

    Add files

--- src/com/shifr/Main.java
+++ src/com/shifr/Main.java
@@ -1,20 +1,29 @@
 package com.shifr;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 	// write your code here
         int key = 7;
         String alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ.,\"\":- ?!;";
-
-
-        while (true){
-            System.out.println("If you want to encrypt? Please press - 1");
-            System.out.println("Enter the key - ");
-            System.out.println("If you want to decrypt? Please press - 2");
-            System.out.println("Enter the key - ");
-            if (1)
+        Path path = Path.of("C:\\Users\\Ihor Nikolaiev\\Text for project.txt");
+        List<String> list = Files.readAllLines(path);
+        for (String s : list) {
+            System.out.println(s);
+        }
+//
+//        while (true){
+//            System.out.println("If you want to encrypt? Please press - 1");
+//            System.out.println("Enter the key - ");
+//            System.out.println("If you want to decrypt? Please press - 2");
+//            System.out.println("Enter the key - ");
+//            if (1)
 
         }
     }
-}
+
