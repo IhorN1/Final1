bc56e7cbfe3076750a13a5af4089d9e5e09b3105
Ihor Nikolaiev <igorexa.ua@gmail.com>
2022-01-23 19:24:36 +0200

    Created arraylist

--- src/com/shifr/Main.java
+++ src/com/shifr/Main.java
@@ -3,6 +3,7 @@ package com.shifr;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -10,18 +11,32 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 	// write your code here
-        System.out.println("Hi, friend! Please, etner your key and push Enter");
+        String alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ.,\"\":- ?!;";
+        System.out.println("Hi, friend! If you want encrypt press 1 and Enter. If you want crack press 2 and Enter");
+        Scanner b = new Scanner(System.in);
+        int programm = b.nextInt();
+        System.out.println("Friend! Please, choose your key and push Enter");
         Scanner a = new Scanner(System.in);
         int key = a.nextInt();
-        String alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ.,\"\":- ?!;";
         System.out.println("Friend! Please, input address your file and push Enter");
         Scanner s = new Scanner(System.in);
         String str = s.nextLine();
-        Path path = Path.of(str);
-        List<String> list = Files.readAllLines(path);
-        for (String c : list) {
+//        Path path = Path.of(str);
+        ArrayList<String> arrayList = new ArrayList<>(Files.readAllLines(Path.of(str)));
+        for (String c : arrayList) {
             System.out.println(c);
         }
+        if (programm == 1){
+
+
+
+
+        } else if (programm == 2){
+
+
+
+
+        } else;
 
 //
 //        while (true){
