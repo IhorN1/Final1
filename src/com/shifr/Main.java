74d383fe444ff560800e2c84f75c748b16728141
Ihor Nikolaiev <igorexa.ua@gmail.com>
2022-01-25 20:05:03 +0200

    I working to 1

--- src/com/shifr/Main.java
+++ src/com/shifr/Main.java
@@ -10,46 +10,43 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-	// write your code here
+        // write your code here
         String alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ.,\"\":- ?!;";
         char[] charsAlphabet = alphabet.toCharArray();
+        char[] result = new char[charsAlphabet.length];
         System.out.println("Hi, friend! If you want encrypt press 1 and Enter \n" + "If you want crack for key press 2 and Enter \n" + "If you want crack text for method Brute force press 3 and Enter \n" + "If you want crack text for method Cryptostatistic press 4 and Enter");
         Scanner b = new Scanner(System.in);
         int programm = b.nextInt();
-        System.out.println("Friend! Please, choose your key and push Enter");
-        Scanner a = new Scanner(System.in);
-        int key = a.nextInt();
-        System.out.println("Friend! Please, input address your file and push Enter");
-        Scanner s = new Scanner(System.in);
-        String str = s.nextLine();
-//        Path path = Path.of(str);
-        ArrayList<String> arrayList = new ArrayList<>(Files.readAllLines(Path.of(str)));
-        char[] charsArrayList = arrayList.toString().toCharArray();
-        for (int i = 0; i < charsArrayList.length; i++) {
-           char charsArray = charsArrayList[i];
-            for (int j = 0; j < charsAlphabet.length; j++) {
+//        System.out.println("Friend! Please, choose your key and push Enter");
+//        Scanner a = new Scanner(System.in);
+//        int keyA = a.nextInt();
 
+        if (programm == 1) {
+            System.out.println("Friend! Please, choose your key and push Enter");
+            Scanner a = new Scanner(System.in);
+            int keyA = a.nextInt();
+            int keyB = charsAlphabet.length + keyA;
+            System.out.println("Friend! Please, input address your file and push Enter");
+            Scanner s = new Scanner(System.in);
+            String str = s.nextLine();
+            ArrayList<String> arrayList = new ArrayList<>(Files.readAllLines(Path.of(str)));
+            char[] charsArrayList = arrayList.toString().toCharArray();
+            for (int i = 0; i < charsArrayList.length; i++) {
+                char charsArray = charsArrayList[i];
+                for (int j = 0; j < charsAlphabet.length; j++) {
+                    char charsAlp = charsAlphabet[i];
+                    if (charsArray == charsAlp) {
+                        result[i] = charsAlphabet[(i + keyB) % charsAlphabet.length];
+                    }
+                }
             }
-        }
-//        char[] arrayListChars = arrayList.
-//        for (String c : arrayList) {
-//            System.out.println(c);
-//        }
-        if (programm == 1){
+            System.out.println(new String(result));
 
 
+        } else if (programm == 2) {
 
 
-            System.out.println();
-
-
-
-        } else if (programm == 2){
-
-
-
-
-        } else;
+        } else ;
 
 //
 //        while (true){
@@ -59,6 +56,6 @@ public class Main {
 //            System.out.println("Enter the key - ");
 //            if (1)
 
-        }
     }
+}
 
